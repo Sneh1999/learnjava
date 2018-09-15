415ee5aeb26ed57af958f4b9bc94b3818d805077
Sneh <snehkoul1999@gmail.com>
2018-09-15 00:03:38 -0400

    SecondCommit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,13 @@
 package Main;
 
 public class Main {
+    public static void main(String[] args){
+        String myName = "Paulo";
+        String lasName = "jkals";
+
+
+        int age = 31;
+        System.out.print(age);
+
+    }
 }
